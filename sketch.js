48b08d43f8fdd4b265c86c19becb1a629b4cfd6e
seankoko98 <78056052+seankoko98@users.noreply.github.com>
2021-03-23 20:03:47 -0700

    added files

---
+++ sketch.js
@@ -0,0 +1,17 @@
+/***********************************************************************************
+  PROJECT NAME HERE
+  by YOUR NAME
+  
+  DESCRIPTION HERE
+***********************************************************************************/
+
+// Setup code goes here
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+}
